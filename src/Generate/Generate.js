1a42c7d2b47670cb36ee24f2012d3f5054d86ec8
Nolan Caine <nolancaine@gmail.com>
2021-11-11 21:12:04 -0700

    Create basic components

---
+++ src/Generate/Generate.js
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react';
+
+const Generate = () => {
+
+  return (
+    <section>
+      <h1>generate!</h1>
+    </section>
+  )
+}
+
+export default Generate;
---
+++ src/Saved/Saved.js
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react';
+
+const Saved = () => {
+
+  return (
+    <section>
+      <h1>saved!</h1>
+    </section>
+  )
+}
+
+export default Saved;
