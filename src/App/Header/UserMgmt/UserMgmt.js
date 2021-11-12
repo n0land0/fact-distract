7c1a3b4ef1cfad6a056aadbff0e11cfa1cfe677a
Nolan Caine <nolancaine@gmail.com>
2021-11-11 23:11:38 -0700

    Import subcomponents

--- src/App/Header/UserMgmt/UserMgmt.js
+++ src/App/Header/UserMgmt/UserMgmt.js
@@ -1,10 +1,21 @@
 import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import { AppContext } from '../../AppContextProvider';
+import LanguageSelect from './LanguageSelect/LanguageSelect';
+import PaletteMode from './PaletteMode/PaletteMode';
 
 const UserMgmt = () => {
+  // const {
+  //   currentFact, recentFacts, savedFacts, paletteMode, language, allLanguages,
+  //   setCurrentFact, setRecentFacts, setSavedFacts, setPaletteMode, setLanguage
+  // } = useContext(AppContext);
 
   return (
-    <h2>Hello, User!</h2>
+    <article class='user-mgmt-container'>
+      <h2>Hello, User!</h2>
+      <LanguageSelect />
+      <PaletteMode />
+    </article>
   )
 }
 
