9829f06c02bcc74ae70c2a7f718518e0809848f2
Nolan Caine <nolancaine@gmail.com>
2021-11-11 22:10:05 -0700

    Add global state values

--- src/App/AppContextProvider.js
+++ src/App/AppContextProvider.js
@@ -1,15 +1,42 @@
 import React, { createContext, useState, useEffect, useReducer } from 'react';
+import fetchCalls from '../fetchCalls';
 
 export const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
-  // hooks
+  const [ currentFact, setCurrentFact ] = useState('');
+  const [ recentFacts, setRecentFacts ] = useState([
+    "Swag thundercats 3 wolf moon, mumblecore paleo pop-up pickled vegan fixie before they sold out tbh",
+    "Offal jianbing palo santo, shabby chic semiotics vape organic art party affogato poutine vice vinyl banh mi.",
+    "Chicharrones drinking vinegar tilde master cleanse snackwave adaptogen DIY tattooed"
+  ]);
+  const [ savedFacts, setSavedFacts ] = useState([]);
+  const [ paletteMode, setPaletteMode ] = useState('dark');
+  const [ language, setLanguage ] = useState('en');
+
+  const value = {
+    currentFact, recentFacts, savedFacts, paletteMode,
+    setCurrentFact, setRecentFacts, setSavedFacts, setPaletteMode
+  }
+
+  useEffect(() => {
+    fetchCalls.getNewFact(language)
+      .then(newFact => setCurrentFact(newFact))
+  }, [])
 
   return (
-    <AppContext.Provider value={''}>
+    <AppContext.Provider value={value}>
       { children }
     </AppContext.Provider>
   )
 }
 
 export default AppContextProvider;
+
+
+// import AppContext from
+//
+// const {
+//   currentFact, recentFacts, savedFacts, paletteMode,
+//   setCurrentFact, setRecentFacts, setSavedFacts, setPaletteMode
+// } = useContext(AppContext);
