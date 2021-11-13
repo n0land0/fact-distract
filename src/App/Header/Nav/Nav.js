cc75489dee91085ef034187aed573e74ff0a4097
Nolan Caine <nolancaine@gmail.com>
2021-11-13 00:50:53 -0700

    Add class names to links

--- src/App/Header/Nav/Nav.js
+++ src/App/Header/Nav/Nav.js
@@ -5,10 +5,10 @@ const Nav = () => {
 
   return (
     <nav className='navbar'>
-      <NavLink to='/' className='navbar__navlink' activeClassName='navlink-active'>
+      <NavLink to='/' className='navbar__navlink generate' activeClassName='navlink-active'>
         new fact
       </NavLink>
-      <NavLink to='/saved' className='navbar__navlink' activeClassName='navlink-active'>
+      <NavLink to='/saved' className='navbar__navlink saved' activeClassName='navlink-active'>
         saved facts
       </NavLink>
     </nav>
