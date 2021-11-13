55e3cfefead0df1af1d788a0b51cff96f7c52292
Nolan Caine <nolancaine@gmail.com>
2021-11-13 00:57:51 -0700

    Add header tests

---
+++ cypress/integration/header.spec.js
@@ -0,0 +1,38 @@
+describe('header', () => {
+  const url = 'localhost:3000';
+
+  before(() => {
+    cy.visit(url)
+  })
+
+  beforeEach(() => {
+
+  })
+
+  it('should show a logo', () => {
+    cy.get('.turkey-logo')
+      .should('be.visible')
+  })
+
+  it('should show nav links to new fact and saved facts pages', () => {
+    cy.get('.navbar__navlink')
+      .should(($navlink) => {
+        expect($navlink).to.have.length(2)
+      })
+  })
+
+  it('should navigate to saved facts page', () => {
+    cy.get('.saved').click()
+      .url().should('include', '/saved')
+  })
+
+  it('should navigate to new fact page', () => {
+    cy.get('.generate').click()
+      .url().should('not.include', '/saved')
+  })
+
+  it('should show user management settings', () => {
+    cy.get('.user-mgmt-container')
+      .should('be.visible')
+  })
+})
