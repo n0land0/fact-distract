709cc2bfc299d6a91ebb503cbe49a6b1c607ce36
Nolan Caine <nolancaine@gmail.com>
2021-11-12 22:44:24 -0700

    Add classNames

--- src/Generate/RecentFacts/RecentFacts.js
+++ src/Generate/RecentFacts/RecentFacts.js
@@ -12,7 +12,8 @@ const RecentFacts = () => {
 
   return (
     <section className='recent-facts-container'>
-      { recentFact1 && <article className='recent-fact-1'>
+      <h2 className='recent-facts__header'>recent facts</h2>
+      { recentFact1 && <article className='recent-fact-1__container'>
         <p className='recent-fact-1__text'>{ recentFact1 }</p>
         <button
           className='recent-fact__button save-fact'
@@ -21,7 +22,7 @@ const RecentFacts = () => {
           { buttonText1 }
         </button>
       </article> }
-      { recentFact2 && <article className='recent-fact-2'>
+      { recentFact2 && <article className='recent-fact-2__container'>
         <p className='recent-fact-2__text'>{ recentFact2 }</p>
         <button
           className='recent-fact__button save-fact'
@@ -30,7 +31,7 @@ const RecentFacts = () => {
           { buttonText2 }
         </button>
       </article> }
-      { recentFact3 && <article className='recent-fact-3'>
+      { recentFact3 && <article className='recent-fact-3__container'>
         <p className='recent-fact-3__text'>{ recentFact3 }</p>
         <button
           className='recent-fact__button save-fact'
--- src/Saved/SavedFacts/SavedFacts.js
+++ src/Saved/SavedFacts/SavedFacts.js
@@ -9,6 +9,7 @@ const SavedFacts = () => {
 
   return (
     <section className='saved-facts-container'>
+      <h2 className='saved-facts__header'>my saved facts</h2>
       { savedFactItems }
     </section>
   )
