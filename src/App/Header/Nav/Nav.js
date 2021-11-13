2029f70bde671c906c0fb8a8a35d7260cd4cc695
Nolan Caine <nolancaine@gmail.com>
2021-11-13 00:28:06 -0700

    Change landing url

--- src/App/Header/Nav/Nav.js
+++ src/App/Header/Nav/Nav.js
@@ -5,7 +5,7 @@ const Nav = () => {
 
   return (
     <nav className='navbar'>
-      <NavLink to='/generate' className='navbar__navlink' activeClassName='navlink-active'>
+      <NavLink to='/' className='navbar__navlink' activeClassName='navlink-active'>
         new fact
       </NavLink>
       <NavLink to='/saved' className='navbar__navlink' activeClassName='navlink-active'>
